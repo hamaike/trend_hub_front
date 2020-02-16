72443c055568daae00de5be5177f3db154e332a2
HamaikeYusuke <hamaike@HamaikeYusukenoMacBook-Pro.local>
2020-02-16 23:03:09 +0900

    add hedgehog

--- src/components/AppBar/index.js
+++ src/components/AppBar/index.js
@@ -6,6 +6,8 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Hedgehog from '../../assets/hedgehog.svg'
+import style from './appBar.module.scss'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -27,7 +29,8 @@ export default function ButtonAppBar() {
       <AppBar position="static">
         <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
+            {/*<MenuIcon />*/}
+            <img src={Hedgehog} className={style.logo}/>
           </IconButton>
           <Typography variant="h4" className={classes.title}>
             Trend Hub
